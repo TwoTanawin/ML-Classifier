8941155add0cdef509fc2770b8b11c257eefd8d7
twoise <tanawin.tw011@gmail.com>
2023-12-16 18:24:18 +0700

    v1

---
+++ src/torch-svm-nonLinear.py
@@ -0,0 +1,57 @@
+import torch
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Convert data to PyTorch tensors
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32)
+
+# Move data to GPU if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+X_train, y_train, X_test, y_test = X_train.to(device), y_train.to(device), X_test.to(device), y_test.to(device)
+
+# Train the non-linear SVM model using RBF kernel
+svm = SVC(kernel="rbf", decision_function_shape="ovo", gamma="scale")
+svm.fit(X_train.cpu().numpy(), y_train.cpu().numpy())
+
+# Make predictions on the test set
+predictions = torch.tensor(svm.predict(X_test.cpu().numpy()), dtype=torch.float32)
+
+# Convert predictions to numpy array
+predictions_np = predictions.cpu().numpy()
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test.cpu().numpy(), predictions_np)
+print(f"Accuracy: {accuracy * 100:.2f}%")
+
+# Calculate confusion matrix
+conf_matrix = confusion_matrix(y_test.cpu().numpy(), predictions_np)
+
+# Display the confusion matrix using seaborn heatmap
+class_names = iris.target_names
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.show()
